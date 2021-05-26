a203b66f589f31c55ae2d16a213a627ff96d00ad
Paul <>
2021-05-26 23:09:39 +0000

    Added Red and Dog buttons

--- Button.js
+++ Button.js
@@ -1,9 +1,19 @@
+function cat()
+{
+	document.getElementById("animal").innerHTML="cat";
+}
+
 function green()
 {
 	document.getElementById("color").style.color="green";
 }
 
-function cat()
+function dog()
 {
-	document.getElementById("animal").innerHTML="cat";
+	document.getElementById("animal").innerHTML="dog";
+}
+
+function red()
+{
+	document.getElementById("color").style.color="red";
 }
